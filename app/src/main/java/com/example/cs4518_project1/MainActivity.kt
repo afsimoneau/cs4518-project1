ef9aa1fd1b0f28625c743c077e5b312c49c2aa27
afreglett <45523745+afreglett@users.noreply.github.com>
2021-09-08 10:37:57 -0400

    ButtonListeners

--- app/src/main/java/com/example/cs4518_project1/MainActivity.kt
+++ app/src/main/java/com/example/cs4518_project1/MainActivity.kt
@@ -9,7 +9,11 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button;
+import android.widget.TextView
+import android.widget.Toast
 import com.example.cs4518_project1.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,16 +22,55 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        var button3a = findViewById<Button>(R.id.button3a)
+        var button3b = findViewById<Button>(R.id.button3b)
+        var button2b = findViewById<Button>(R.id.button2b)
+        var button2a = findViewById<Button>(R.id.button2a)
+        var FreeThrowB = findViewById<Button>(R.id.FreeThrowB)
+        var FreeThrowA = findViewById<Button>(R.id.FreeThrowA)
+        var ResetButt = findViewById<Button>(R.id.ResetButt)
+        val TeamBScore: TextView = findViewById(R.id.TeamBScore)
+        val TeamAScore = findViewById<TextView>(R.id.TeamAScore)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        button3a.setOnClickListener{
+            var num: Int = Integer.valueOf(TeamAScore.getText().toString())
+            num += 3
+            TeamAScore.text = num.toString()
         }
+        button2a.setOnClickListener{
+            var num: Int = Integer.valueOf(TeamAScore.getText().toString())
+            num += 2
+            TeamAScore.text = num.toString()
+        }
+        FreeThrowA.setOnClickListener{
+            var num: Int = Integer.valueOf(TeamAScore.getText().toString())
+            num += 1
+            TeamAScore.text = num.toString()
+        }
+        button3b.setOnClickListener{
+            var num: Int = Integer.valueOf(TeamBScore.getText().toString())
+            num += 3
+            TeamBScore.text = num.toString()
+        }
+        button2b.setOnClickListener{
+            var num: Int = Integer.valueOf(TeamBScore.getText().toString())
+            num += 2
+            TeamBScore.text = num.toString()
+        }
+        FreeThrowB.setOnClickListener{
+            var num: Int = Integer.valueOf(TeamBScore.getText().toString())
+            num += 1
+            TeamBScore.text = num.toString()
+        }
+        ResetButt.setOnClickListener{
+            TeamAScore.text = "0"
+            TeamBScore.text = "0"
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
