1e2d86f13cea3e10989e013cd40e42ca6e1aad17
Alex Simoneau <afsimoneau@gmail.com>
2021-09-15 21:20:10 -0400

    bug is fixed. do not know why

--- app/src/androidTest/java/com/example/cs4518_project/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/cs4518_project/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.cs4518_project1", appContext.packageName)
+        assertEquals("com.example.cs4518_projects", appContext.packageName)
     }
 }
