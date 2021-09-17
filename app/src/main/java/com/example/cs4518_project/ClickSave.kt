a0e7a01b66f886d723ea786e5ed02ac6a2e9f553
afreglett <45523745+afreglett@users.noreply.github.com>
2021-09-17 12:01:11 -0400

    Part2 Complete

--- app/src/main/java/com/example/cs4518_project/ClickSave.kt
+++ app/src/main/java/com/example/cs4518_project/ClickSave.kt
@@ -1,15 +1,19 @@
 package com.example.cs4518_project
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
+private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
 
 class ClickSave : AppCompatActivity() {
     private lateinit var teamBScore: TextView
     private lateinit var teamAScore: TextView
     private lateinit var teamAText: TextView
     private lateinit var teamBText: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,10 @@ class ClickSave : AppCompatActivity() {
         val savedScoreB = intent.getStringExtra(scoreB)
         val savedTextA = intent.getStringExtra(teamAName)
         val savedTextB= intent.getStringExtra(teamBName)
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, true)
+        }
+        setResult(Activity.RESULT_OK, data)
 
         teamAScore = findViewById<TextView>(R.id.TeamAScore).apply { text= savedScoreA }
         teamBScore = findViewById<TextView>(R.id.TeamBScore).apply { text= savedScoreB }
@@ -25,5 +33,6 @@ class ClickSave : AppCompatActivity() {
         teamBText = findViewById<TextView>(R.id.TeamBText).apply { text= savedTextB }
 
     }
-    }
+
+}
 
